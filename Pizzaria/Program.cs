1634c0522e2e275f96d76debf025b7fbf8ecc1f6
DevBruno <brunoantonio.dev0016@gmail.com>
2024-12-27 17:57:23 -0300

    feat(Program): define que a IPizzaInterface vai enxergar e pode utilizar os métodos da classe PizzaService

--- Pizzaria/Program.cs
+++ Pizzaria/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
+using Pizzaria.Controllers;
 using Pizzaria.Data;
+using Pizzaria.Services.Pizza;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +12,8 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddScoped<IPizzaInterface, PizzaService>();
 
 var app = builder.Build();
 
