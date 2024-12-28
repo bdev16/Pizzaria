e88710ab7989c1d8549603b965351ee1953d052a
DevBruno <brunoantonio.dev0016@gmail.com>
2024-12-28 15:42:25 -0300

    feat: Cria o modelo do método CriarPizza dentro da Interface IPizzaInterface

--- Pizzaria/Services/Pizza/IPizzaInterface.cs
+++ Pizzaria/Services/Pizza/IPizzaInterface.cs
@@ -1,6 +1,10 @@
-﻿namespace Pizzaria.Services.Pizza
+﻿using Pizzaria.Dto;
+using Pizzaria.Models;
+
+namespace Pizzaria.Services.Pizza
 {
     public interface IPizzaInterface
     {
+        Task<PizzaModel> CriarPizza(PizzaCriacaoDto pizzaCriacaoDto, IFormFile foto);
     }
 }
