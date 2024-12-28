ada83ce2667f4419d3ef4b1b141c6b7326fd4a2d
DevBruno <brunoantonio.dev0016@gmail.com>
2024-12-28 18:19:34 -0300

    feat(Controllers/PizzaController): Cria o método Detalhes

--- Pizzaria/Controllers/PizzaController.cs
+++ Pizzaria/Controllers/PizzaController.cs
@@ -25,6 +25,13 @@ namespace Pizzaria.Controllers
             return View();
         }
 
+        public async Task<IActionResult> Detalhes (int id)
+        {
+
+            var pizza = await _pizzaInterface.GetPizzaPorId(id);
+            return View(pizza);
+        }
+
         public async Task<IActionResult> Editar(int id)
         {
             var pizza = await _pizzaInterface.GetPizzaPorId(id);
