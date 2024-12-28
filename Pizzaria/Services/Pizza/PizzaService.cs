7a01fa9510ee181b9ddcecb0c4694d0167f0280f
DevBruno <brunoantonio.dev0016@gmail.com>
2024-12-28 15:46:13 -0300

    feat: Cria o método GerarCaminhoArquivo dentro da classe PizzaService

--- Pizzaria/Services/Pizza/PizzaService.cs
+++ Pizzaria/Services/Pizza/PizzaService.cs
@@ -1,6 +1,39 @@
-﻿namespace Pizzaria.Services.Pizza
+﻿using Pizzaria.Data;
+using Pizzaria.Dto;
+using Pizzaria.Models;
+using System.Runtime.CompilerServices;
+
+namespace Pizzaria.Services.Pizza
 {
-    public class PizzaService : IPizzaInterface
+    public class PizzaService
     {
+        private readonly string _sistema;
+        public PizzaService(IWebHostEnvironment sistema)
+        {
+            _sistema = sistema.WebRootPath;
+        }
+
+        public string GeraCaminhoArquivo(IFormFile imagem)
+        {
+            var codigoUnico = Guid.NewGuid().ToString();
+            var nomeCaminhoImagem = imagem.FileName.Replace(" ", "").ToLower() + codigoUnico + ".png";
+
+            var caminhoParaSalvarImagens = _sistema + "\\imagem\\";
+
+
+            if (!Directory.Exists(caminhoParaSalvarImagens))
+            {
+                Directory.CreateDirectory(caminhoParaSalvarImagens);
+            }
+
+            using (var stream = File.Create(caminhoParaSalvarImagens + nomeCaminhoImagem))
+            {
+                imagem.CopyToAsync(stream).Wait();
+            }
+
+            return nomeCaminhoImagem;
+
+
+        }
     }
 }
