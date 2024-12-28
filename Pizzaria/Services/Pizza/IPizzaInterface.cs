990234d7b585c1ec6d947a8309c40546e740c2af
DevBruno <brunoantonio.dev0016@gmail.com>
2024-12-28 17:33:09 -0300

    feat(Services/Pizza/IPizzaInterface): Cria o modelo do método Editar

--- Pizzaria/Services/Pizza/IPizzaInterface.cs
+++ Pizzaria/Services/Pizza/IPizzaInterface.cs
@@ -8,5 +8,6 @@ namespace Pizzaria.Services.Pizza
         Task<PizzaModel> CriarPizza(PizzaCriacaoDto pizzaCriacaoDto, IFormFile foto);
         Task<List<PizzaModel>> GetPizzas();
         Task<PizzaModel> GetPizzaPorId(int id);
+        Task<PizzaModel> EditarPizza(PizzaModel pizza, IFormFile? foto);
     }
 }
