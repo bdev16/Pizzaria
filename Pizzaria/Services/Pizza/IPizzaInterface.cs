c76082c911a1cb7e8187814d8999f8ad3dccbd10
DevBruno <brunoantonio.dev0016@gmail.com>
2024-12-28 17:48:29 -0300

    feat(Services/Pizza/PizzaService): Implementa o método Remover presente na IPizzaInterface

--- Pizzaria/Services/Pizza/IPizzaInterface.cs
+++ Pizzaria/Services/Pizza/IPizzaInterface.cs
@@ -9,5 +9,6 @@ namespace Pizzaria.Services.Pizza
         Task<List<PizzaModel>> GetPizzas();
         Task<PizzaModel> GetPizzaPorId(int id);
         Task<PizzaModel> EditarPizza(PizzaModel pizza, IFormFile? foto);
+        Task<PizzaModel> RemoverPizza(int id);
     }
 }
