2a391ab14609b78840503279941bbf1059db2e4f
DevBruno <brunoantonio.dev0016@gmail.com>
2024-12-28 15:51:48 -0300

    feat: Cria o método Cadastrar dentro da classe PizzaController

--- Pizzaria/Controllers/PizzaController.cs
+++ Pizzaria/Controllers/PizzaController.cs
@@ -1,9 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Pizzaria.Dto;
+using Pizzaria.Services.Pizza;
 
 namespace Pizzaria.Controllers
 {
     public class PizzaController : Controller
     {
+        private readonly IPizzaInterface _pizzaInterface;
+
+        public PizzaController(IPizzaInterface pizzaInterface)
+        {
+            _pizzaInterface = pizzaInterface;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -12,6 +21,20 @@ namespace Pizzaria.Controllers
         public IActionResult Cadastrar()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Cadastrar(PizzaCriacaoDto pizzaCriacaoDto, IFormFile imagem)
+        {
+            if (ModelState.IsValid)
+            {
+                var pizza = await _pizzaInterface.CriarPizza(pizzaCriacaoDto, imagem);
+                return RedirectToAction("Index", "Pizza");
+            }
+            else
+            {
+                return View(pizzaCriacaoDto);
+            }
         }
     }
 }
